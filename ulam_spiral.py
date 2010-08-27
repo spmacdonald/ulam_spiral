2815506c508b7ee1343485eb4083c6229af3d3bb
Scott MacDonald <scott.macdonald@bonsorta.com>
2010-08-26 19:20:50 -0700

    Compute x,y coordinates for any odd n.

--- ulam_spiral.py
+++ ulam_spiral.py
@@ -1,17 +1,15 @@
 from itertools import chain
 
 # Square length must be odd.
-x_coords = [0]
+x_coords = [[0]]
+y_coords = [[0]]
 for i, n in enumerate(xrange(3, 9, 2)):
     seq1 = [n-(2+i) for x in xrange(n)]
     seq2 = [x-(i+1) for x in reversed(xrange(n))]
     seq3 = [(2+i)-n for x in xrange(n)]
     seq4 = [x-(i+1) for x in xrange(n)]
-
-    print seq1
-    print seq2
-    print seq3
-    print seq4
     x_coords.append(seq1[1:] + seq2[1:] + seq3[1:] + seq4[1:])
+    y_coords.append(seq4[1:] + seq1[1:] + seq2[1:] + seq3[1:])
 
 print list(chain.from_iterable(x_coords))
+print list(chain.from_iterable(y_coords))
