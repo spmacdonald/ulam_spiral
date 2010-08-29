52da2a0fdc09e76d4a7abe2eac3ea1dd3d55b7d3
Scott MacDonald <scott.macdonald@bonsorta.com>
2010-08-28 18:20:32 -0700

    Fixed indexing bug.

--- grid_sizing_tests.py
+++ grid_sizing_tests.py
@@ -9,9 +9,9 @@ def is_power_of_2(n):
 def factor_gen(n):
     return set(reduce(list.__add__, ([i, n/i] for i in xrange(1, int(sqrt(n) + 1)) if n % i == 0)))
 
-for square_size in range(1, 10):
+for square_size in range(1, 500):
     d1 = defaultdict(int)
-    for i in range(1, square_size**2):
-        d1[sum(factor_gen(i))] += 1
+    for i in range(1, square_size**2 + 1):
+        d1[len(factor_gen(i))] += 1
 
-    print square_size**2, len(d1), is_power_of_2(len(d1))
+    print square_size, len(d1), is_power_of_2(len(d1))
